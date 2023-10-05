57541fb012c75f25f83168e960af5da5cf3d69b9
Taras Priadka <taras.priadka@gmail.com>
2023-10-05 11:42:03 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,26 +6,16 @@ ZSHRC_PATH="zshrc"
 
 cd $HOME
 
-# install code highlighting for zsh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/taras/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install fzf
+$(brew --prefix)/opt/fzf/install
+
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# install code completetion for zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# install ag searcher
-apt-get install silversearcher-ag
-
-# install golang
-wget -O https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-
-# install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# install python
-apt-get install python
-apt-get install pip
 
 # Copy vimrc and zshrc files to home
 curl -O $GIT_REPO/$VIMRC_PATH 
